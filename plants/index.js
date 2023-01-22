9bd01c924c221c81505a620ad2cf79d6b1152291
andrey28121989 <anenashev89@gmail.com>
2023-01-22 14:12:19 +0400

    feat: adaptiv mobile header and price

--- plants/index.js
+++ plants/index.js
@@ -1,1 +1,14 @@
-console.log(50)
+document.addEventListener('DOMContentLoaded', ()=> {
+    const burger = document.querySelector('.burger');
+    const navigation = document.querySelector('.navigation');
+
+
+console.log({burger})
+console.log({navigation})
+    burger.addEventListener('click', ()=> {
+    navigation.classList.toggle('active')
+    burger.classList.toggle('opened')
+    })  
+})
+
+
