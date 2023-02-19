e9fdb866254a65fbfdfce9508ac0ced6220c631f
andrey28121989 <anenashev89@gmail.com>
2023-02-20 02:59:43 +0400

    feat: weather

--- momentum/main.js
+++ momentum/main.js
@@ -48,22 +48,70 @@ function showGreeting() {
 }
 showGreeting()
 
-// Name
+// Name and city
 
 const name = document.querySelector('.name');
+const city = document.querySelector('.city');
 
 function setLocalStorage() {
-  localStorage.setItem('name', name.value);
+  localStorage.setItem('name', name.value)
+  localStorage.setItem('city', city.value);
 }
 window.addEventListener('beforeunload', setLocalStorage)
 
 function getLocalStorage() {
   if(localStorage.getItem('name')) {
     name.value = localStorage.getItem('name');
+    city.value = localStorage.getItem('city');
   }
 }
 window.addEventListener('load', getLocalStorage)
 
 // Background Image
 
-body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
+// body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
+
+// Weather
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const weatherError = document.querySelector('.weather-error');
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=a33a5a5addebaf28899dcd5ec475902d&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json(); 
+  
+  if (data.cod === '404' || data.cod === '400') {
+    // weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = '';
+    weatherDescription.textContent = '';
+    wind.textContent = '';
+    humidity.textContent = '';
+    weatherError.textContent = 'Incorrect city specified';
+    weatherIcon.className = 'weather-icon owf';
+  } else {
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    // weatherIcon.className = 'weather-icon owf';
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    weatherError.textContent = '';
+  }
+  }
+    
+getWeather();
+
+function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+    city.blur();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
