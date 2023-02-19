d4a478f84c6c0585edf2fdaa81288718ce7b0f1e
andrey28121989 <anenashev89@gmail.com>
2023-02-19 23:28:52 +0400

    feat: welcome and name

--- momentum/main.js
+++ momentum/main.js
@@ -18,3 +18,52 @@ function showTime() {
   }
   showTime();
 
+// welcome
+
+const greeting = document.querySelector('.greeting');
+
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours(); 
+  let timeOfDay;
+
+  if (hours >= 6 && hours < 12) {
+    timeOfDay = 'morning';
+ } else if (hours >= 12 && hours < 18) {
+  timeOfDay = 'afternoon';
+ } else if (hours >= 18 && hours < 24) {
+  timeOfDay = 'evening';
+ } else {
+  timeOfDay = 'night';
+ }
+
+ return timeOfDay;
+}
+getTimeOfDay()
+
+function showGreeting() {
+  const timeOfDay = getTimeOfDay();
+  const greetingText = `Good ${timeOfDay}`;
+  greeting.textContent = greetingText;
+}
+showGreeting()
+
+// Name
+
+const name = document.querySelector('.name');
+
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage)
+
+// Background Image
+
+body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
